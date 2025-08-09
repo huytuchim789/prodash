ecb310e782780d5237733ea9be12a93f299e0884
loki-mfxdev <tran.tu.huy@moneyforward.co.jp>
2025-08-09 18:13:04 +0900

    Add visualizer

--- rollup.config.esm.js
+++ rollup.config.esm.js
@@ -1,7 +1,17 @@
 import { baseConfig } from './rollup.config.base.js';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default {
   ...baseConfig,
+  plugins: [
+    ...baseConfig.plugins,
+    visualizer({
+      open: process.env.STATS === 'true',
+      filename: 'stats.html',
+      title: 'Prodash Bundle Analysis',
+      template: 'treemap', // 'treemap', 'sunburst', 'network'
+    }),
+  ],
   output: {
     dir: 'dist/esm',
     format: 'es',
